b97b6eacacfb09492590149e83d520e42bf67864
niko057 <nicatnamazov57.nn@gmail.com>
2024-04-15 08:32:14 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Reflections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student ali = new Student();
+            Console.WriteLine(ali.Age); 
+
+            Student murad = new Student();
+            Console.WriteLine(murad.Age);
+
+            Student gadir = new Student();
+            Console.WriteLine(gadir.Age);
+
+            Student nicat = new Student();
+            Console.WriteLine(nicat.Age);
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflections
+{
+    public class Student:Person
+    {
+        public Student()
+        {
+            lastAge++;
+            Age= lastAge;
+        }
+    }
+}
